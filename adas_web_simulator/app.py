d899a983c7ba44030eb2dd57326d40844f2999d4
DESKTOP-H86FBO9\Kim <ehdnjs0623@naver.com>
2024-12-17 16:12:39 +0900

    24.12.17(화) 16.11 app.py 코드수정, index.html 구조수정

--- adas_web_simulator/app.py
+++ adas_web_simulator/app.py
@@ -1,18 +1,10 @@
-from flask import Flask, render_template, request
-import os
-import signal
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
-
-@app.route('/shutdown', methods=['POST'])
-def shutdown():
-    # Flask 애플리케이션을 종료하는 함수
-    os.kill(os.getpid(), signal.SIGINT)
-    return "서버가 종료되었습니다."
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5002)
